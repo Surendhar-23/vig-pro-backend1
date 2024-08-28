1d3433c8b0187b62d6bb91d8369e82639c73f101
SURENDHAR N D <ndsurendhar2004@gmail.com>
2024-08-28 23:30:39 +0530

    first commit

--- index.js
+++ index.js
@@ -15,7 +15,13 @@ const app = express();
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: "*" }));
+app.use(
+  cors({
+    origin: "*", // Allows all origins; for security, replace '*' with specific domains in production
+    methods: "GET,POST,PUT,DELETE,OPTIONS",
+    allowedHeaders: "Content-Type, Authorization",
+  })
+);
 
 connectdb();
 
